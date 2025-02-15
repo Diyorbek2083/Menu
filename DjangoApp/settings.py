115712b88c366376b22928ae26283fea1b320a50
Diyorbek2083 <begmamatovdiyorbek14@gmail.com>
2025-02-15 23:23:52 +0500

    Birinchi yuklanish

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0%iv)f#%)!lgph=-snxx-@n1%luu!g94iwx&i#4tw_7z2!+8%2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MenyuApi',
+    'UserApi',
 ]
 
 MIDDLEWARE = [
