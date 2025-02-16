9ef4fafdf37580c0cfefd08c545691e90ae83885
abdulaziz8886 <abdulazizilhomboyev2@gmail.com>
2025-02-16 20:30:55 +0500

    for git

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
-urlpatterns = []
+from .views import PostApi
+from .documentation import schema_view
+urlpatterns = [
+    path('', PostApi.as_view())
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import AllowAny 
+from django.contrib.auth.models import User
+from .serializers import ModelSelializer
+class PostApi(ListAPIView):
+    
+    permission_classes = (AllowAny, )
+    queryset = User.objects.all()
+    serializer_class = ModelSelializer
