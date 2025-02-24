876ef10b7e9ea7bd2afad3f29d2e75a1a626eafd
abdulaziz8886 <abdulazizilhomboyev2@gmail.com>
2025-02-24 22:27:48 +0500

    For git

--- app/admin.py
+++ app/admin.py
@@ -7,8 +7,13 @@ admin.site.register(models.TableModels)
 admin.site.register(models.WaiterModels)
 admin.site.register(models.FoodCategoryaModels)
 admin.site.register(models.CommentModel)
+
+
+
 admin.site.register(models.ReplayCommentModels)
 admin.site.register(models.LikeMolels)
 admin.site.register(models.KarzinkaModels)
 admin.site.register(models.DoimiyKarzinkaModels)
 admin.site.register(models.UserModels)
+
+
